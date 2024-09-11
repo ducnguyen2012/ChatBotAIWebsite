80b0908e8a865c823cd08f8bfca6288d9757f840
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2024-09-11 03:03:56 +0100

    Update Home.jsx

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
       setMessages([...messages, { text: input, sender: 'user' }]);
       setIsThinking(true);
       try {
-        const response = await fetch('http://127.0.0.1:5000/', {
+        const response = await fetch('http://127.0.0.1:4000/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
